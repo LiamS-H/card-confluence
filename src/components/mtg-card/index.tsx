152aadf369def3703f40e35ca7aabad909dee476
liamsh@gmail.com <liamsh@gmail.com>
2024-11-09 00:50:23 -0800

    add error and loading card

--- src/components/mtg-card/index.tsx
+++ src/components/mtg-card/index.tsx
@@ -2,6 +2,6 @@ import MTGTypography from '../mtg-typography';
 import { Scrycard } from 'react-scrycards';
 import { ScryfallCard } from '@scryfall/api-types';
 
-export default function MTGCard(props: { card: ScryfallCard.Any }) {
+export default function MTGCard(props: { card: ScryfallCard.Any | null | undefined }) {
     return <Scrycard card={props.card} symbol_text_renderer={MTGTypography} size='lg' animated />;
 }
