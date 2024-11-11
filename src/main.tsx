063e9e7bf770e72f193fc9b8c4b97c922d7ea987
liamsh@gmail.com <liamsh@gmail.com>
2024-11-11 00:01:59 -0800

    add lightmode

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import Root from './routes/root';
+import Router from './routes';
 import 'react-scrycards/dist/index.css';
 
 const queryClient = new QueryClient();
@@ -14,7 +14,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <Root />
+            <Router />
             <ReactQueryDevtools />
         </QueryClientProvider>
     </React.StrictMode>,
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Home from './home';
 import { useMemo, useState } from 'react';
-import { CssBaseline } from '@mui/material';
-import Error from './error';
-import About from './about';
+import { CssBaseline, IconButton, Tooltip } from '@mui/material';
+import { Brightness4, Brightness7, Info, Search } from '@mui/icons-material';
 
 export default function Root() {
     const [theme, setTheme] = useState<'light' | 'dark'>('dark');
+    const location = useLocation();
     const MUItheme = useMemo(
         () =>
             createTheme({
@@ -17,17 +16,54 @@ export default function Root() {
             }),
         [theme],
     );
-    setTheme;
+
+    const handleThemeToggle = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+
     return (
         <ThemeProvider theme={MUItheme}>
             <CssBaseline />
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' Component={Home} />
-                    <Route path='/about' Component={About} />
-                    <Route path='*' Component={Error} />
-                </Routes>
-            </BrowserRouter>
+            <Tooltip title='click to toggle dark theme.'>
+                <IconButton
+                    onClick={handleThemeToggle}
+                    sx={{
+                        position: 'absolute',
+                        top: 16,
+                        right: 16,
+                        color: 'inherit',
+                    }}
+                >
+                    {theme === 'dark' ? <Brightness7 /> : <Brightness4 />}
+                </IconButton>
+            </Tooltip>
+            {location.pathname != '/about' ? (
+                <IconButton
+                    href={'/about'}
+                    sx={{
+                        position: 'absolute',
+                        top: 16,
+                        left: 16,
+                        color: 'inherit',
+                    }}
+                >
+                    <Info />
+                </IconButton>
+            ) : null}
+            {location.pathname != '/' ? (
+                <IconButton
+                    href={'/'}
+                    sx={{
+                        position: 'absolute',
+                        top: 16,
+                        left: 16,
+                        color: 'inherit',
+                    }}
+                >
+                    <Search />
+                </IconButton>
+            ) : null}
+            <Outlet />
         </ThemeProvider>
     );
 }
