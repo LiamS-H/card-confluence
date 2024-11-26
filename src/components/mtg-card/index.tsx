36aaa3c1158b195aea9118a1c1b2b89c5d8b726a
liamsh@gmail.com <liamsh@gmail.com>
2024-11-26 03:04:06 -0800

    update flip button UI

--- src/components/mtg-card/index.tsx
+++ src/components/mtg-card/index.tsx
@@ -1,25 +1,49 @@
 import MTGTypography from '../mtg-typography';
-import { Scrycard } from 'react-scrycards';
+import { isFlippable, Scrycard } from 'react-scrycards';
 import { ScryfallCard } from '@scryfall/api-types';
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+import FlipIcon from '@mui/icons-material/Flip';
 
 export default function MTGCard(props: {
     card: ScryfallCard.Any | null | undefined;
     size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }) {
+    const [flipped, setFlipped] = useState<boolean>(false);
     const card = (
         <Scrycard
             card={props.card}
             symbol_text_renderer={MTGTypography}
             size={props.size ?? 'lg'}
             animated
+            flipped={flipped}
+            imageLink='auto'
         />
     );
-    if (!props.card?.scryfall_uri) {
+    if (!props.card) {
         return card;
     }
+
     return (
-        <a href={props.card.scryfall_uri} target='_blank' rel='noopener noreferrer'>
+        <>
             {card}
-        </a>
+            {isFlippable(props.card) ? (
+                <IconButton
+                    sx={{
+                        position: 'absolute',
+                        width: '14%',
+                        left: '0%',
+                        bottom: '1%',
+                        '& svg': {
+                            transform: flipped ? 'scaleX(-1)' : 'scaleX(1)',
+                            transition: 'transform 0.3s ease',
+                        },
+                    }}
+                    onClick={() => setFlipped((f) => !f)}
+                >
+                    <FlipIcon />
+                </IconButton>
+            ) : null}
+        </>
     );
 }
