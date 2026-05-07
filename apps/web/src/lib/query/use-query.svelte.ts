4c92d4b379bde2de236ed15146628cd26dace6c7
LiamS-H <liamsh@gmail.com>
2026-05-07 13:43:27 -0700

    fix old response resets when awaiting debounce

--- apps/web/src/lib/query/use-query.svelte.ts
+++ apps/web/src/lib/query/use-query.svelte.ts
@@ -2,21 +2,22 @@ import { query_client, query_to_string, type QueryRequest } from '$lib/query/cli
 
 export function use_query(getQuery: () => QueryRequest, debounce: number) {
 	let timeout: NodeJS.Timeout;
+	let key = $state(query_to_string(getQuery()));
 
 	$effect(() => {
 		query_client.track_invalidations();
 		const query = getQuery();
-		if (debounce) {
-			timeout = setTimeout(() => query_client.ensure_query(query), debounce);
-		} else {
+		const next_key = query_to_string(query);
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			key = next_key;
 			query_client.ensure_query(query);
-		}
+		}, debounce);
 		return () => clearTimeout(timeout);
 	});
 
 	return {
 		get response() {
-			const key = query_to_string(getQuery());
 			return query_client.queries.get(key) ?? { loading: true, error: false };
 		}
 	};
